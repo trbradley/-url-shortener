ef94d42c04f292e4a526bfdcdea8b68a74e4b043
Thomas Bradley <bradley.tom.r@gmail.com>
2016-02-22 12:19:12 +0000

    First feature test

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+ruby '2.3.0'
+
+gem 'rake'
+gem 'sinatra'
+gem 'sinatra-flash'
+gem 'sinatra-partial'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+
+group :test do
+  gem 'rspec'
+  gem 'capybara'
+  gem 'byebug'
+  gem 'database_cleaner'
+end
---
+++ spec/features/shorten_url_spec.rb
@@ -0,0 +1,12 @@
+# As a user
+# So that I don't have to type long URLs
+# I want to enter a long URL and get a shortened version back
+
+feature 'Providing shortenend urls' do
+  scenario 'when url entered in form' do
+    visit '/links/new'
+    fill_in 'url', with: 'https://www.google.co.uk/#q=I+need+a+url+shortener'
+    click_button 'Shorten'
+    expect(page).to have_selector('#short_link', text: 'http://localhost:1337/random')
+  end
+end
